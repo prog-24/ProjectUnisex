db4e9c1e9f50e446e7e21c068f5cba205469e20e
Promise Ekoriko <pekoriko@live.com>
2012-10-26 02:51:11 +0100

    removing_old files

---
+++ confess.php
@@ -0,0 +1,6 @@
+<?php require("includes/config.php");
+?>
+<form action="actions/post_confession.php" method="post">
+<textarea name="confess" cols="30" rows="20" wrap="virtual" class="confess" id="conf"></textarea>
+<button type="submit" value="confess">Confess</button>
+</form>
--- home.php
+++ home.php
@@ -3,3 +3,12 @@ $browser = $_SERVER['HTTP_USER_AGENT'];
 
 
 ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+</head>
+<body>
+<?php include("confess.php"); ?>
+</body>
+</html>
